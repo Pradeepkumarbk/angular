af25501c4f117c4aa0732f9c2fa218b6c4bfd5c9
Pradeep Kumar B.K <pradeepkumar.bk96@gmail.com>
2021-12-02 00:08:32 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -22,12 +22,18 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+    ChromeHeadlessNoSandbox: {
+    base: "ChromeHeadless",
+    flags: ["--no-sandbox"]
+  }
+},
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false
   });
 };
